8700fc252cee8e1529e31e145ca659424e1dbd88
Ryan Allen <allenrya@umich.edu>
2025-04-17 00:35:14 -0400

    update footer

--- frontend/app/components/common/footer.tsx
+++ frontend/app/components/common/footer.tsx
@@ -1,29 +1,38 @@
-import { Box, Link } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 
 export default function Footer() {
   return (
     <Box
       component='footer'
       sx={{
-        backgroundColor: 'transparent',
         display: 'flex',
-        justifyContent: 'right',
-        gap: 1,
+        justifyContent: 'space-between',
         px: 2,
-        opacity: 0.5,
       }}
     >
-      <Link href='https://cosmic-tracer-ui.vercel.app/'>
-        <small>Nebula Interactive Gaming, {new Date().getFullYear()}</small>
-      </Link>
-      |
-      <Link href='https://umdearborn.edu'>
-        <small>University of Michigan-Dearborn, CIS 376</small>
-      </Link>
-      |
-      <Link href='https://github.com/RyanAIIen/cis-376-cosmic-tracer'>
-        <small>Code</small>
-      </Link>
+      <Typography color='#aaa'>
+        This app is for educational purposes only.
+      </Typography>
+
+      <Typography
+        sx={{
+          display: 'flex',
+          gap: 1,
+          opacity: 0.5,
+        }}
+      >
+        <Link href='https://cosmic-tracer-ui.vercel.app/'>
+          <small>Cosmic Tracer, {new Date().getFullYear()}</small>
+        </Link>
+        |
+        <Link href='https://umdearborn.edu'>
+          <small>University of Michigan-Dearborn CIS-376</small>
+        </Link>
+        |
+        <Link href='https://github.com/RyanAIIen/cis-376-cosmic-tracer'>
+          <small>View the Code</small>
+        </Link>
+      </Typography>
     </Box>
   );
 }
