cc426ff847f0ef99cb6e49c0110d7cf2e4333afc
Ryan Allen <allenrya@umich.edu>
2025-04-17 03:46:28 -0400

    revert commented out require auth

--- frontend/app/components/utils/RequireAuth.tsx
+++ frontend/app/components/utils/RequireAuth.tsx
@@ -2,23 +2,15 @@
 
 import Container from '@mui/material/Container';
 import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
 
 import { LoadingSpinner } from '@/app/components/common';
 import { useAppSelector } from '@/redux/hooks';
-import { useRouter } from 'next/navigation';
 
 interface RequireAuthProps {
   children: React.ReactNode;
 }
 
 export default function RequireAuth({ children }: RequireAuthProps) {
-  // TEMPORARY FIX: Bypass auth check and always render content
-  // Remove this for production!
-  return <Container maxWidth='xl'>{children}</Container>;
-  
-  // Original code below - commented out temporarily
-  /*
   const { isLoading, isAuthenticated } = useAppSelector((state) => state.auth);
 
   if (isLoading) {
@@ -30,5 +22,4 @@ export default function RequireAuth({ children }: RequireAuthProps) {
   }
 
   return <Container maxWidth='xl'>{children}</Container>;
-  */
 }
